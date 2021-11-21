4345ede905c2e128ae3e682793c16c7cc0805668
TheKing0fBears <90875696+TheKing0fBears@users.noreply.github.com>
2021-11-20 22:37:58 -0800

    Update core.py

--- core.py
+++ core.py
@@ -11,9 +11,10 @@ class Node:
   def make_children(self, current_layer_number, node_per_layer_map):
     if current_layer_number >= len(node_per_layer_map):
       return
-    for i in range(node_per_layer_map[current_layer_number]):
-      self.children.append(Node())
-    self.children[0].make_children(current_layer_number+1, node_per_layer_map)
+    for i in range(node_per_layer_map[current_layer_number] ):
+      self.children.append( Node() )
+   
+   self.children[0].make_children(current_layer_number+1, node_per_layer_map)
     
    for i in range(1, len(self.children)):
       self.children[i].children = self.children[0].children[:]
