981f685c5d4fa673e40f48a66c2918efb6ffcee8
TheKing0fBears <90875696+TheKing0fBears@users.noreply.github.com>
2021-11-21 20:35:42 -0800

    Update core.py

--- core.py
+++ core.py
@@ -9,14 +9,17 @@ class Node:
     self.node_name = ''.join([random.choice(string.ascii_letters) for i in range(3)])
     
   def make_children(self, current_layer_number, node_per_layer_map):
+    #when to terminate
     if current_layer_number >= len(node_per_layer_map):
       return
+    
     for i in range(node_per_layer_map[current_layer_number] ):
       self.children.append( Node() )
    
-      self.children[0].make_children(current_layer_number+1, node_per_layer_map)
     
-      for i in range(1, len(self.children)):
+    self.children[0].make_children(current_layer_number+1, node_per_layer_map)
+    
+    for i in range(1, len(self.children)):
          self.children[i].children = self.children[0].children[:]
    
   def printout(self,current_layer_number,node_per_layer_map):
